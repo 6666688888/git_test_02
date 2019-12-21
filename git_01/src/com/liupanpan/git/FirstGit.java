517de75c82ae5a9f913cc5a15e5a9be40f55acdf
admin <499256826@qq.com>
2019-12-21 19:46:09 +0800

    把本地工厂添加到git

--- git_01/src/com/liupanpan/git/FirstGit.java
+++ git_01/src/com/liupanpan/git/FirstGit.java
@@ -29,10 +29,15 @@ public class FirstGit {
         System.out.println("hello world");
         System.out.println("hello world");
         test();
+        test2();
     }
 
     public static void test() {
         System.out.println("0-----");
+    }
+
+    public static void test2() {
+        System.out.println("0000000000000000000000");
     }
 
 }
