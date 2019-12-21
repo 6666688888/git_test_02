1e7f92454a196d02b91475a848bc69e67a2590af
admin <499256826@qq.com>
2019-12-21 19:44:59 +0800

    把本地工厂添加到git

---
+++ git_01/src/com/liupanpan/git/FirstGit.java
@@ -0,0 +1,38 @@
+package com.liupanpan.git;
+
+public class FirstGit {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        test();
+    }
+
+    public static void test() {
+        System.out.println("0-----");
+    }
+
+}
